6cf8b4a07613dbb5ed23e5920568ed1ddd363534
MdNoyon-wed <islamnoyon1272008@gmail.com>
2025-02-28 23:25:22 +0600

    first commit

---
+++ index/js/script.js
@@ -0,0 +1,11 @@
+ document.getElementById("main-card").addEventListener("click",function(event){
+
+  event.preventDefault();
+   window.location.href="next.html"
+
+   
+  const allMineBtn =document.querySelectorAll(". btn-completed")
+  console.log(allMineBtn)
+ });
+
+  
