08cd40acd8a170a0cc89b3db15c8e8c954626ff3
MdNoyon-wed <islamnoyon1272008@gmail.com>
2025-03-02 16:19:05 +0600

    js problem

--- index/js/script.js
+++ index/js/script.js
@@ -1,11 +1,42 @@
- document.getElementById("main-card").addEventListener("click",function(event){
 
-  event.preventDefault();
-   window.location.href="next.html"
+const allBtn = document.getElementsByClassName("comon-class")
+//  console.log(allBtn)
 
-   
-  const allMineBtn =document.querySelectorAll(". btn-completed")
-  console.log(allMineBtn)
- });
+ 
 
+
+
+// console.log(convertedNumber)
   
+for(let i = 0 ; i < allBtn.length ; i++){
+
+ allBtn[i].addEventListener("click",function(){
+ 
+  const firstNumber = document.getElementById("first-number").innerText;
+
+  const converted = parseFloat(firstNumber);
+  //  console.log(converted)
+
+  const seandNumber =document.getElementById("seand-Number").innerText;
+  // console.log(seandNumber)
+  const convertedNumber = parseFloat(seandNumber);
+
+  const number = converted +1;
+
+  const number1 = convertedNumber -1 ;
+
+  // console.log(number,number1).innerText;
+
+  if(number,number1){
+
+    alert("done");
+
+    }else{
+      [i].seAttribute('disabled',true)
+    }
+  
+
+   })
+}
+ 
+ 
