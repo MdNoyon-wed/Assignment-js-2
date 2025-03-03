53c69e1ead298116e4f799cf1bf896f4464b1c64
MdNoyon-wed <islamnoyon1272008@gmail.com>
2025-03-03 09:02:45 +0600

    bg-color'

--- index/js/script.js
+++ index/js/script.js
@@ -3,40 +3,53 @@ const allBtn = document.getElementsByClassName("comon-class")
 //  console.log(allBtn)
 
  
-
-
-
 // console.log(convertedNumber)
   
 for(let i = 0 ; i < allBtn.length ; i++){
 
- allBtn[i].addEventListener("click",function(){
- 
-  const firstNumber = document.getElementById("first-number").innerText;
+  // console.log(allBtn)
 
+  // allBtn[i]
+
+ allBtn[i].addEventListener("click",function(){
+  // console.log(allBtn[i])
+ 
+   
+
+  const firstNumber = document.getElementById("first-number").innerText;
   const converted = parseFloat(firstNumber);
-  //  console.log(converted)
+  //  console.log(typeof converted)
+
 
   const seandNumber =document.getElementById("seand-Number").innerText;
-  // console.log(seandNumber)
-  const convertedNumber = parseFloat(seandNumber);
+  const convetedNumber = parseFloat(seandNumber);
+  //  console.log(typeof convetedNumber)
 
-  const number = converted +1;
+  const number = converted ;
 
-  const number1 = convertedNumber -1 ;
+  const number1 = convetedNumber ;
 
   // console.log(number,number1).innerText;
 
-  if(number,number1){
+  if(number,number1,allBtn[i]){
+     
+    const sum = allBtn[i] = number +1 ;
 
-    alert("done");
+    document.getElementById("first-number").innerText = sum;
 
-    }else{
-      [i].seAttribute('disabled',true)
+    const sum1 =allBtn[i] = number1 -1 ;
+
+    document.getElementById("seand-Number").innerText = sum1;
+
+     alert("Board updated Succesfuly")
+
+
     }
-  
+
+
 
    })
+
 }
  
  
